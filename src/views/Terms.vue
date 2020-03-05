a44cbc425eaf9f10bdc2e7b77da9fe14029e0ad7
soe-j <soeda@val.co.jp>
2020-03-06 01:22:32 +0900

    add Terms page

---
+++ src/views/Terms.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="terms">
+    <h1>This is an terms page</h1>
+    <p>
+      まあそういうことだ
+    </p>
+
+    <div class="inner">
+      <p>
+        どういうことだ
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Terms'
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2rem;
+}
+</style>
