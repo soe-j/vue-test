5ece36153deb35f9e5d0406c91d5f81869ac22b7
soe-j <soeda@val.co.jp>
2020-03-24 00:12:17 +0900

    use routes name

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/terms">Terms</router-link>
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link> |
+      <router-link :to="{ name: 'Terms'}">Terms</router-link>
     </div>
     <router-view/>
   </div>
